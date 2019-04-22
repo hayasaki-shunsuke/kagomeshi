97806cd7f4a10c8b222e3a38a2a59b2022cc0733
Masaki Komagata <komagata@gmail.com>
2019-04-22 18:54:43 +0900

    コメントのfixturesを追加
    トップページのコメント数も追加。

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,7 +4,7 @@ class RestaurantsController < ApplicationController
   # GET /restaurants
   # GET /restaurants.json
   def index
-    @restaurants = Restaurant.all
+    @restaurants = Restaurant.order(:created_at)
   end
 
   # GET /restaurants/1
