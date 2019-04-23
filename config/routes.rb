c9f5683a6c4b4ba99522bcb5f2f74e6a827dd8b6
Masaki Komagata <komagata@gmail.com>
2019-04-23 14:36:56 +0900

    ユーザーの個別ページを追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   devise_for :users
   resources :restaurants
   resources :comments
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: %i(show)
 end
